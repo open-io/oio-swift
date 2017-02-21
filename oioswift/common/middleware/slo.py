6fd5202bd5400125a4e12b729b829652cc10ea1c
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-02-21 14:34:14 +0100

    slo: make the generated etag compatible with S3

--- oioswift/common/middleware/slo.py
+++ oioswift/common/middleware/slo.py
@@ -3,6 +3,7 @@ from swift.common.utils import config_true_value, \
     override_bytes_from_content_type
 from swift.common.middleware.slo import filter_factory as slo_filter_factory
 from swift.common.middleware.slo import SloGetContext
+from binascii import unhexlify
 
 
 def get_or_head_response(self, req, resp_headers, resp_iter):
@@ -11,10 +12,7 @@ def get_or_head_response(self, req, resp_headers, resp_iter):
         etag = md5()
         content_length = 0
         for seg_dict in segments:
-            if seg_dict.get('range'):
-                etag.update('%s:%s;' % (seg_dict['hash'], seg_dict['range']))
-            else:
-                etag.update(seg_dict['hash'])
+            etag.update(unhexlify(seg_dict['hash']))
 
             if config_true_value(seg_dict.get('sub_slo')):
                 override_bytes_from_content_type(
