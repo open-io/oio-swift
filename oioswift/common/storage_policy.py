e2a0f48e2f572fa0c74cf2906a5fefa430221664
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-07-04 17:52:12 +0200

    Read header X-Oio-Storage-Policy for storage policy
    The policy specified by "X-Oio-Storage-Policy" header takes precedence
    over the policy determined from the "Content-Length". Notice that the
    policy must have been declared in "oio_storage_policies" configuration
    parameter.

--- oioswift/common/storage_policy.py
+++ oioswift/common/storage_policy.py
@@ -16,7 +16,7 @@
 from swift.common import storage_policy
 
 default_policies = [
-    storage_policy.StoragePolicy(0, 'Policy-0', True)
+    storage_policy.StoragePolicy(0, 'SINGLE', True)
 ]
 
 POLICIES = storage_policy.StoragePolicyCollection(default_policies)
