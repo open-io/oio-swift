5f1ddfdacdf18d5997c383e261db74b6e51686cc
Florent Vennetier (OpenIO) <florent@fridu.net>
2018-02-14 17:00:59 +0100

    Implement container_hierarchy middleware
    This middleware will spawn a container for each virtual subdirectory
    found in object path.
    
    For example:
     PUT /v1/account/container/obj1/obj2
    will create an object "obj2" in container "container%2Fobj1" instead
    of creating an object "obj1/obj2" in container "container".

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
             'hashedcontainer=oioswift.common.middleware.hashedcontainer:filter_factory',
             'regexcontainer=oioswift.common.middleware.regexcontainer:filter_factory',
             'versioned_writes=oioswift.common.middleware.versioned_writes:filter_factory',
+            'container_hierarchy=oioswift.common.middleware.container_hierarchy:filter_factory',
         ],
     },
     install_requires=['swift>=2.13.0', 'oio>=4.1.0.a0']
