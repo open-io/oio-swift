6bd6bb4f4c113d6862d9f7f21c98248af85e69c7
Florent Vennetier (OpenIO) <florent@fridu.net>
2018-03-08 10:06:57 +0100

    container_hierarchy: improve test script

--- tests/functional/s3_container_hierarchy.sh
+++ tests/functional/s3_container_hierarchy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 GW_NETLOC="127.0.0.1:5000"
+#GW_NETLOC="127.0.0.1:5001"
 BUCKET="bucket0"
 OBJ_SRC="/etc/resolv.conf"
 AWS_CMD="aws --endpoint-url http://${GW_NETLOC} --no-verify-ssl"
+#AWS_CMD="aws --endpoint-url http://${GW_NETLOC} --no-verify-ssl --profile keystone"
 
 set -e
 
@@ -14,6 +16,49 @@ echo "Uploading some objects in bucket ${BUCKET}"
 $AWS_CMD s3 cp "${OBJ_SRC}" "s3://${BUCKET}/file_at_root"
 $AWS_CMD s3 cp "${OBJ_SRC}" "s3://${BUCKET}/subdir1/file_in_subdir1"
 $AWS_CMD s3 cp "${OBJ_SRC}" "s3://${BUCKET}/subdir1/subdir2/file_in_subdir2"
+
+echo "Uploading multipart object in bucket ${BUCKET}/subdir1/multi_in_subdir1"
+MULTI_FILE=$(mktemp -t container_hierarchy_XXXXXX.dat)
+dd if=/dev/zero of="${MULTI_FILE}" count=21 bs=1M
+$AWS_CMD s3 cp "${MULTI_FILE}" "s3://${BUCKET}/subdir1/multi_in_subdir1"
+rm "${MULTI_FILE}"
+
+echo "Checking existing objects (HEAD)"
+$AWS_CMD s3api head-object --bucket "${BUCKET}" --key "file_at_root"
+$AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/file_in_subdir1"
+$AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/subdir2/file_in_subdir2"
+
+echo "Checking existing objects with trailing slashes (HEAD)"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "file_at_root/" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/file_in_subdir1/" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/subdir2/file_in_subdir2/" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+
+echo "Checking directory placeholders (HEAD)"
+$AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/"
+$AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/subdir2/"
+
+echo "Checking invalid objects (HEAD)"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "invalid_at_root" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/invalid_in_subdir1" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/subdir2/invalid_in_subdir2" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/invalid_dir/file_in_invalid_dir" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+
+echo "Checking invalid objects with trailing slashes (HEAD)"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "invalid_at_root/" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/invalid_in_subdir1/" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/subdir2/invalid_in_subdir2/" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
+HEAD_OUT=$($AWS_CMD s3api head-object --bucket "${BUCKET}" --key "subdir1/invalid_dir/file_in_invalid_dir/" 2>&1 | tail -n 1)
+echo "${HEAD_OUT}" | grep "Not Found"
 
 echo "Listing objects at root of bucket ${BUCKET}"
 LS_OUT=$($AWS_CMD s3 ls "s3://${BUCKET}")
@@ -30,10 +75,20 @@ set +e
 read -d '' EXPECTED << EOF
 file_at_root
 subdir1/file_in_subdir1
+subdir1/multi_in_subdir1
 subdir1/subdir2/file_in_subdir2
 EOF
 set -e
 
 [ "${LS_REC_OUT}" = "${EXPECTED}" ]
+
+$AWS_CMD s3 rm "s3://${BUCKET}/file_at_root"
+$AWS_CMD s3 rm "s3://${BUCKET}/subdir1/file_in_subdir1"
+$AWS_CMD s3 rm "s3://${BUCKET}/subdir1/multi_in_subdir1"
+$AWS_CMD s3 rm "s3://${BUCKET}/subdir1/subdir2/file_in_subdir2"
+
+$AWS_CMD s3 rm "s3://${BUCKET}/subdir1/subdir2/"
+$AWS_CMD s3 rm "s3://${BUCKET}/subdir1/"
+$AWS_CMD s3 rb "s3://${BUCKET}/"
 
 echo "OK"
