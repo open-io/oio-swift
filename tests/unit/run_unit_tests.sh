aba15c227c597d9374f66e6107ecec5995780831
Michael Bonfils <murlock42@gmail.com>
2018-04-20 16:33:17 +0200

    test: add unit test for container hierarchy

--- tests/unit/run_unit_tests.sh
+++ tests/unit/run_unit_tests.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-coverage run --source=oioswift -a $(which nosetests) -v tests/unit/controllers
 coverage run --source=oioswift -a $(which nosetests) -v \
+    tests/unit/controllers \
     tests/unit/common/middleware/test_copy.py:TestOioServerSideCopyMiddleware \
-    tests/unit/common/middleware/test_versioned_writes.py:OioVersionedWritesTestCase
+    tests/unit/common/middleware/test_versioned_writes.py:OioVersionedWritesTestCase \
+    tests/unit/common/middleware/test_container_hierarchy.py
 
 ./oio-check-version.sh
