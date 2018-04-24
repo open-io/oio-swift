29ddc23b53d7075ce963d9f8d27d27d312c9c5a9
Michael Bonfils <murlock42@gmail.com>
2018-04-24 13:03:52 +0200

    tests: add regexcontainer

--- tests/unit/run_unit_tests.sh
+++ tests/unit/run_unit_tests.sh
@@ -6,6 +6,7 @@ coverage run --source=oioswift -a $(which nosetests) -v \
     tests/unit/controllers \
     tests/unit/common/middleware/test_copy.py:TestOioServerSideCopyMiddleware \
     tests/unit/common/middleware/test_versioned_writes.py:OioVersionedWritesTestCase \
-    tests/unit/common/middleware/test_container_hierarchy.py
+    tests/unit/common/middleware/test_container_hierarchy.py \
+    tests/unit/common/middleware/test_regexcontainer.py
 
 ./oio-check-version.sh
