17d9ba7d9eb31a8058bf394323bde2decb484691
Michael Bonfils <bonfils.michael@protonmail.com>
2018-12-03 21:46:59 +0100

    Add LD_LIBRARY_PATH for unit test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,6 +8,7 @@ if [ "$TEST_SUITE" = "unit" ]
 then
   tests/unit/run_unit_tests.sh
 else
+  export LD_LIBRARY_PATH=/tmp/oio/lib:$LD_LIBRARY_PATH
   tests/functional/run-${TEST_SUITE}-tests.sh $*
   tests/unit/run_extra_unit_tests.sh
 fi
