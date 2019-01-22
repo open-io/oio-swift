8f612f7496476a5b03143a88c09601ab90d3d5d1
Michael Bonfils <bonfils.michael@protonmail.com>
2019-01-22 23:00:05 +0100

    Update inherit ACL tests

--- tests/functional/s3-acl-inherit.sh
+++ tests/functional/s3-acl-inherit.sh
@@ -17,11 +17,11 @@ ${AWS} s3api create-bucket --bucket ${BUCKET}
 # This should not be readable by user tester2 because no acl is specified
 ${AWS} s3api put-object --bucket ${BUCKET} --key 'unreadable-by-tester2' --body /etc/passwd
 echo 'This should not be readable by user tester2 because no acl is specified'
-data=$(${AWS} s3api head-object --bucket ${BUCKET} --key 'unreadable-by-tester2' --profile tester2 1&>2)
+data=$(${AWS} s3api head-object --bucket ${BUCKET} --key 'unreadable-by-tester2' --profile tester2 2>&1 || true)
 grep "Forbidden" <<< $data || ( echo $data ; exit 1)
 
 # New acl to permit new object to readable by user tester2
-${AWS} s3api put-object-acl --bucket ${BUCKET} --grant-read id=tester2:tester2 --grant-full-control id=admin:admin
+${AWS} s3api put-bucket-acl --bucket ${BUCKET} --grant-read id=tester2:tester2 --grant-full-control id=admin:admin
 
 # File is readable by user tester2
 echo 'File is readable by user tester2'
@@ -30,35 +30,41 @@ data=$(${AWS} s3api head-object --bucket ${BUCKET} --key 'readable-by-tester2' -
 grep "Forbidden" <<< $data && ( echo $data ; exit 1)
 
 # Remove read permission to user tester2 the bucket
-${AWS} s3api put-object-acl --bucket ${BUCKET} --grant-full-control id=admin:admin
+${AWS} s3api put-bucket-acl --bucket ${BUCKET} --grant-full-control id=admin:admin
 
 # Old file is still readable
 echo 'Old file is still readable'
-data=$(${AWS} s3api head-object --bucket ${BUCKET} --key 'readable-by-tester2' --profile tester2)
-grep "Forbidden" <<< $data && ( echo $data ; exit 1)
+${AWS} s3api head-object --bucket ${BUCKET} --key 'readable-by-tester2' --profile tester2
 
 # New file is unreadable
 echo 'New file is unreadable'
 ${AWS} s3api put-object --bucket ${BUCKET} --key 'unreadable-by-tester22' --body /etc/passwd
-data=$(${AWS} s3api head-object --bucket ${BUCKET} --key 'unreadable-by-tester22' --profile tester2)
+data=$(${AWS} s3api head-object --bucket ${BUCKET} --key 'unreadable-by-tester22' --profile tester2 2>&1 || true)
 grep "Forbidden" <<< $data || ( echo $data ; exit 1)
 
 # New file is not modifiable
 echo 'New file is not modifiable'
 ${AWS} s3api put-object --bucket ${BUCKET} --key 'unmodifiable-by-tester22' --body /etc/passwd
-data=$(${AWS} s3api put-object --bucket ${BUCKET} --key 'unmodifiable-by-tester22' --body /etc/passwd --profile tester2)
-grep "Forbidden" <<< $data || ( echo $data ; exit 1)
+data=$(${AWS} s3api put-object --bucket ${BUCKET} --key 'unmodifiable-by-tester22' --body /etc/passwd --profile tester2 2>&1 || true)
+grep "AccessDenied" <<< $data || ( echo $data ; exit 1)
 
 # New acl to permit new object to readable by user tester2
-${AWS} s3api put-object-acl --bucket ${BUCKET} --grant-write id=tester2:tester2 --grant-full-control id=admin:admin
+${AWS} s3api put-bucket-acl --bucket ${BUCKET} --grant-write id=tester2:tester2 --grant-full-control id=admin:admin
 
 # New file is modifiable
 echo 'New file is modifiable'
 ${AWS} s3api put-object --bucket ${BUCKET} --key 'modifiable-by-tester22' --body /etc/passwd
-data=$(${AWS} s3api put-object --bucket ${BUCKET} --key 'modifiable-by-tester22' --body /etc/passwd --profile tester2)
-grep "Forbidden" <<< $data && ( echo $data ; exit 1)
+${AWS} s3api put-object --bucket ${BUCKET} --key 'modifiable-by-tester22' --body /etc/passwd --profile tester2
 
-# But old file is unmodifiable
-echo 'But old file is unmodifiable'
-data=$(${AWS} s3api put-object --bucket ${BUCKET} --key 'unmodifiable-by-tester22' --body /etc/passwd --profile tester2)
+# But old file is still not readable...
+data=$(${AWS} s3api head-object --bucket ${BUCKET} --key 'unmodifiable-by-tester22' --profile tester2 2>&1 || true)
 grep "Forbidden" <<< $data || ( echo $data ; exit 1)
+
+# But old file is modifiable as permission WRITE was given
+echo 'But old file is modifiable as permission WRITE was given'
+${AWS} s3api put-object --bucket ${BUCKET} --key 'unmodifiable-by-tester22' --body /etc/passwd --profile tester2
+
+echo 'cleanup'
+${AWS} s3api delete-objects --bucket ${BUCKET} --delete 'Objects=[{Key=unreadable-by-tester2},{Key=readable-by-tester2},{Key=modifiable-by-tester22},{Key=unmodifiable-by-tester22},{Key=unreadable-by-tester22}]'
+${AWS} s3api list-objects --bucket ${BUCKET}
+${AWS} s3api delete-bucket --bucket ${BUCKET}
