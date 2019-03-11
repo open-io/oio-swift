892108eee9a9c5558bc6bd4e4401606dc064576d
Michael Bonfils <bonfils.michael@protonmail.com>
2019-03-11 15:12:46 +0100

    CH: use slave for hkeys and hexists

--- oioswift/common/middleware/container_hierarchy.py
+++ oioswift/common/middleware/container_hierarchy.py
@@ -97,13 +97,13 @@ class RedisDb(object):
         return self.conn_slave.scan_iter(pattern, count=5000)
 
     def hkeys(self, key, match=None, count=DEFAULT_LIMIT):
-        return self.conn.hscan_iter(key, match=match, count=count)
+        return self.conn_slave.hscan_iter(key, match=match, count=count)
 
     def exists(self, key):
         return self.conn_slave.exists(key)
 
     def hexists(self, key, hkey):
-        return self.conn.hexists(key, hkey)
+        return self.conn_slave.hexists(key, hkey)
 
 
 class FakeRedis(object):
