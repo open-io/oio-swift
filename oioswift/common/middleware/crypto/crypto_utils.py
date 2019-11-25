4b3b7b2ad264767d28bde40e5c574fdc07449f00
Florent Vennetier <florent@openio.io>
2019-11-25 16:55:57 +0100

    Adapt the crypto code for swift "train"

--- oioswift/common/middleware/crypto/crypto_utils.py
+++ oioswift/common/middleware/crypto/crypto_utils.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2015-2016 OpenStack Foundation
-# Copyright (c) 2018 OpenIO SAS
+# Copyright (c) 2018-2019 OpenIO SAS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,47 +17,7 @@
 from swift.common.middleware.crypto.crypto_utils import Crypto
 from swift.common.swob import header_to_environ_key
 
-try:
-    # Since Queens
-    from swift.common.utils import strict_b64decode
-except ImportError:
-    import base64
-    import binascii
-    import string
-    import six
-
-    def strict_b64decode(value, allow_line_breaks=False):
-        '''
-        Validate and decode Base64-encoded data.
-        The stdlib base64 module silently discards bad characters, but we often
-        want to treat them as an error.
-        :param value: some base64-encoded data
-        :param allow_line_breaks: if True, ignore carriage returns and newlines
-        :returns: the decoded data
-        :raises ValueError: if ``value`` is not a string, contains invalid
-                            characters, or has insufficient padding
-        '''
-        if isinstance(value, bytes):
-            try:
-                value = value.decode('ascii')
-            except UnicodeDecodeError:
-                raise ValueError
-        if not isinstance(value, six.text_type):
-            raise ValueError
-        # b64decode will silently discard bad characters, but we want to
-        # treat them as an error
-        valid_chars = string.digits + string.ascii_letters + '/+'
-        strip_chars = '='
-        if allow_line_breaks:
-            valid_chars += '\r\n'
-            strip_chars += '\r\n'
-        if any(c not in valid_chars for c in value.strip(strip_chars)):
-            raise ValueError
-        try:
-            return base64.b64decode(value)
-        except (TypeError, binascii.Error):  # (py2 error, py3 error)
-            raise ValueError
-
+from swift.common.utils import strict_b64decode
 
 ALGO_HEADER = 'X-Amz-Server-Side-Encryption-Customer-Algorithm'
 KEY_HEADER = 'X-Amz-Server-Side-Encryption-Customer-Key'
